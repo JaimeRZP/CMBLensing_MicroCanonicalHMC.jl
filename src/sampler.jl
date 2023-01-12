424b7868512199a3f8f7402e3204b3d7a497b7e3
jaimerz <jaimerz011235813@gmail.com>
2023-01-12 12:33:31 -0800

    working

--- src/sampler.jl
+++ src/sampler.jl
@@ -58,7 +58,7 @@ function Random_unit_vector(sampler::Sampler)
     """Generates a random (isotropic) unit vector."""
     key = sampler.settings.key
     u = randn(key, sampler.target.d)
-    return  @.(u / sqrt(sum(square(u))))
+    return  @.(u / sqrt(sum(u.^2)))
 
 end
 
@@ -69,23 +69,22 @@ function Partially_refresh_momentum(sampler::Sampler, u)
     key = sampler.settings.key
 
     z = hyperparams.nu .* randn(key, sampler.target.d)
-    uu = @.((u + z) / sqrt(sum(square(u + z))))
+    uu = @.((u + z) / sqrt(sum((u + z)^2)))
     return uu
 end
 
-function Update_momentum(sett::Settings, g, u)
+function Update_momentum(sampler::Sampler, eff_eps, g, u)
     # TO DO: type inputs
     # Have to figure out where and when to define target
     """The momentum updating map of the ESH dynamics (see https://arxiv.org/pdf/2111.02434.pdf)"""
-    eps = sett.eps
 
     g_norm = sqrt.(sum(g.^2))
-    e = .- g ./ g_norm
+    e = - g ./ g_norm
     ue = dot(u, e)
-    sh = sinh.(eps .* g_norm ./ target.d)
-    ch = cosh.(eps .* g_norm ./ target.d)
+    sh = sinh.(eff_eps .* g_norm ./ sampler.target.d)
+    ch = cosh.(eff_eps .* g_norm ./ sampler.target.d)
 
-    return @.(u + e * (sh + ue * (ch - 1))) / (ch + ue * sh)
+    return @.(u + e * (sh + ue * (ch - 1)) / (ch + ue * sh))
 end
 
 function Dynamics(sampler::Sampler, state)
@@ -94,10 +93,10 @@ function Dynamics(sampler::Sampler, state)
     x, u, g, time = state
 
     # Hamiltonian step
-    xx, gg, uu = sampler.hamiltonian_dynamics(x, g, u)
+    xx, gg, uu = sampler.hamiltonian_dynamics(sampler, x, g, u)
 
     # add noise to the momentum direction
-    uuu = Partially_refresh_momentum(uu)
+    uuu = Partially_refresh_momentum(sampler, uu)
 
     return xx, uuu, gg, 0.0
 end
@@ -107,11 +106,11 @@ function Get_initial_conditions(sampler::Sampler; kwargs...)
     target = sampler.target
     sett = sampler.settings
     ### initial conditions ###
-    x = get(kwargs, initial_x, target.prior_draw(sett.key))
+    x = get(kwargs, :initial_x, target.prior_draw(sett.key))
 
     g = @.(target.grad_nlogp(x) * target.d / (target.d - 1))
 
-    u = Random_unit_vector(sett.key) #random initial direction
+    u = Random_unit_vector(sampler) #random initial direction
     #u = - g / jnp.sqrt(jnp.sum(jnp.square(g))) #initialize momentum in the direction of the gradient of log p
 
     return [x, u, g, 0.0]
@@ -134,9 +133,11 @@ function Sample(sampler::Sampler; kwargs...)
                 samples (shape = (num_steps, self.Target.d))
     """
     init = Get_initial_conditions(sampler; kwargs...)
-    samples = zeros(Vector{Vector{eltype(x)}}, kwargs[:num_steps])
+    x, u, g, time = init
+    samples = zeros(eltype(x), length(x), kwargs[:num_steps])
+    samples = Vector{eltype(samples)}[eachcol(samples)...]
     for i in 1:kwargs[:num_steps]
-        init, sample = Step(init)
+        init, sample = Step(sampler, init)
         samples[i] = sample
     end
 
