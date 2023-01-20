388257ce51e4a22f3830f1fd139468047f42f8a8
jaimerz <jaimerz011235813@gmail.com>
2023-01-19 18:35:04 -0800

    turing interface

--- src/targets.jl
+++ src/targets.jl
@@ -1,11 +1,60 @@
-struct StandardGaussianTarget <: Target
+struct Target <: Target
     #TO DO: what types are these?
     d::Int
     variance::Vector{Float64}
-    nlogp
-    grad_nlogp
-    transform
-    prior_draw
+    nlogp::Function
+    grad_nlogp::Function
+    transform::Function
+    prior_draw::Function
+end
+
+function _get_dists(vi)
+    mds = values(vi.metadata)
+    return [md.dists[1] for md in mds]
+end
+
+function TuringTarget(model; kwargs...)
+    # Hack soon to be depricated
+    spl = DynamicPPL.SampleFromPrior()
+    ###
+    ctxt = model.context
+    vi = DynamicPPL.VarInfo(model, context)
+    link!!(vi, model)
+    vsyms = keys(vi)
+    d = length(vsyms)
+
+    ℓ = LogDensityProblemsAD.ADgradient(Turing.LogDensityFunction(vi, model, spl, ctxt))
+    ℓπ = Base.Fix1(LogDensityProblems.logdensity, ℓ)
+    #OPT: we probably want to use this in the future.
+    #     and merge nlogp and grad_nlogp into one function.
+    #∂lπ∂θ(x) = LogDensityProblems.logdensity_and_gradient(ℓ, x)
+
+    function nlogp(x)
+        return ℓπ(x)
+    end
+
+    function grad_nlogp(x)
+        return ForwardDiff.gradient(nlogp, x)
+        #OPT: we probably want to use this in the future.
+        #return ∂lπ∂θ(x)[2]
+    end
+
+    function transform(xs)
+        dists = _get_dists(vi)
+        xxs = [invlink(dist, x) for (dist, x) in zip(dists, xs)]
+        return xxs
+    end
+
+    function prior_draw()
+        return vi[spl]
+    end
+
+    Target(kwargs[:d],
+           ones(d),
+           nlogp,
+           grad_nlogp,
+           transform,
+           prior_draw)
 end
 
 function StandardGaussianTarget(; kwargs...)
@@ -30,10 +79,10 @@ function StandardGaussianTarget(; kwargs...)
         return 4*rand(key, MvNormal(mean, variance))
     end
 
-    StandardGaussianTarget(kwargs[:d],
-                           ones(d),
-                           nlogp,
-                           grad_nlogp,
-                           transform,
-                           prior_draw)
+    Target(kwargs[:d],
+           ones(d),
+           nlogp,
+           grad_nlogp,
+           transform,
+           prior_draw)
 end
