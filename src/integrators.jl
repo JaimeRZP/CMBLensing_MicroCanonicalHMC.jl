2b162e2db49d89cd0f42a3ff63676c0f65ac3807
jaimerz <jaimerz011235813@gmail.com>
2023-01-23 22:57:18 -0800

    Self tuning 2/3 bake

--- src/integrators.jl
+++ src/integrators.jl
@@ -1,38 +1,36 @@
-function Leapfrog(sampler::Sampler, x, g, u)
+function Leapfrog(sampler::Sampler, target::Target, x, g, u)
     sett = sampler.settings
-    target = sampler.target
 
-    uu = Update_momentum(sampler, sett.eps * 0.5, g, u)
+    uu = Update_momentum(sampler, target, sett.eps * 0.5, g, u)
 
     #full step in x
     xx = x .+ sett.eps .* uu
     gg = target.grad_nlogp(xx) .* target.d ./ (target.d - 1)
 
     #half step in momentum
-    uu = Update_momentum(sampler, sett.eps * 0.5, gg, uu)
+    uu = Update_momentum(sampler, target, sett.eps * 0.5, gg, uu)
 
     return xx, gg, uu
 end
 
 
-function Minimal_norm(sampler::Sampler, x, g, u)
+function Minimal_norm(sampler::Sampler, target::Target, x, g, u)
     """Integrator from https://arxiv.org/pdf/hep-lat/0505020.pdf, see Equation 20."""
 
     # V T V T V
     sett = sampler.settings
-    target = sampler.target
 
-    uu = Update_momentum(sampler, sett.eps * sett.lambda_c, g, u)
+    uu = Update_momentum(sampler, target, sett.eps * sett.lambda_c, g, u)
 
     xx = x .+ sett.eps .* 0.5 .* uu
     gg = target.grad_nlogp(xx) .* target.d ./ (target.d - 1)
 
-    uu = Update_momentum(sampler, sett.eps * (1 - 2 * sett.lambda_c), gg, uu)
+    uu = Update_momentum(sampler, target, sett.eps * (1 - 2 * sett.lambda_c), gg, uu)
 
     xx = xx .+ sett.eps .* 0.5 .* uu
     gg = target.grad_nlogp(xx) .* target.d / (target.d - 1)
 
-    uu = Update_momentum(sampler, sett.eps * sett.lambda_c, gg, uu)
+    uu = Update_momentum(sampler, target, sett.eps * sett.lambda_c, gg, uu)
 
     return xx, gg, uu
 end
--- src/tuning.jl
+++ src/tuning.jl
@@ -1,76 +1,80 @@
 function _tuning_step(props)
 
-        key, eps_inappropriate, eps_appropriate, success = props
+    eps_inappropriate, eps_appropriate, success = props
 
-        # get a small number of samples
-        key_new, subkey = jax.random.split(key)
-        X, E = self.sample(samples, x0, subkey, monitor_energy= True)
+    # get a small number of samples
+    E = zeros(eltype(x), length(x), kwargs[:num_steps])
+    E = Vector{eltype(samples)}[eachcol(samples)...]
+    for i in 1:kwargs[:num_steps]
+        init, sample = Step(sampler, target, init)
+        E[i] = sample
+    end
 
-        # remove large jumps in the energy
-        E -= jnp.average(E)
-        E = remove_jumps(E)
+    # remove large jumps in the energy
+    E -= jnp.average(E)
+    E = remove_jumps(E)
 
-        ### compute quantities of interest ###
+    ### compute quantities of interest ###
 
-        # typical size of the posterior
-        x1 = jnp.average(X, axis= 0) #first moments
-        x2 = jnp.average(jnp.square(X), axis=0) #second moments
-        sigma = jnp.sqrt(jnp.average(x2 - jnp.square(x1))) #average variance over the dimensions
+    # typical size of the posterior
+    x1 = jnp.average(X, axis= 0) #first moments
+    x2 = jnp.average(jnp.square(X), axis=0) #second moments
+    sigma = jnp.sqrt(jnp.average(x2 - jnp.square(x1))) #average variance over the dimensions
 
-        # energy fluctuations
-        varE = jnp.std(E)**2 / self.Target.d #variance per dimension
-        no_divergences = np.isfinite(varE)
+    # energy fluctuations
+    varE = jnp.std(E)**2 / self.Target.d #variance per dimension
+    no_divergences = np.isfinite(varE)
 
-        ### update the hyperparameters ###
+    ### update the hyperparameters ###
 
-        if no_divergences:
-            L_new = sigma * jnp.sqrt(self.Target.d)
-            eps_new = self.eps * jnp.power(varE_wanted / varE, 0.25) #assume var[E] ~ eps^4
-            success = jnp.abs(1.0 - varE / varE_wanted) < 0.2 #we are done
+    if no_divergences:
+        L_new = sigma * jnp.sqrt(self.Target.d)
+        eps_new = self.eps * jnp.power(varE_wanted / varE, 0.25) #assume var[E] ~ eps^4
+        success = jnp.abs(1.0 - varE / varE_wanted) < 0.2 #we are done
 
-        else:
-            L_new = self.L
+    else:
+        L_new = self.L
 
-            if self.eps < eps_inappropriate:
-                eps_inappropriate = self.eps
-            end
+        if self.eps < eps_inappropriate:
+            eps_inappropriate = self.eps
         end
-            eps_new = jnp.inf #will be lowered later
+    end
+        eps_new = jnp.inf #will be lowered later
 
 
-        #update the known region of appropriate eps
+    #update the known region of appropriate eps
 
-        if not no_divergences: # inappropriate epsilon
-            if self.eps < eps_inappropriate: #it is the smallest found so far
-                eps_inappropriate = self.eps
-            end
-        else: # appropriate epsilon
-            if self.eps > eps_appropriate: #it is the largest found so far
-                eps_appropriate = self.eps
-            end
+    if not no_divergences: # inappropriate epsilon
+        if self.eps < eps_inappropriate: #it is the smallest found so far
+            eps_inappropriate = self.eps
         end
-
-        # if suggested new eps is inappropriate we switch to bisection
-        if eps_new > eps_inappropriate:
-            eps_new = 0.5 * (eps_inappropriate + eps_appropriate)
+    else: # appropriate epsilon
+        if self.eps > eps_appropriate: #it is the largest found so far
+            eps_appropriate = self.eps
         end
+    end
 
-        self.set_hyperparameters(L_new, eps_new)
+    # if suggested new eps is inappropriate we switch to bisection
+    if eps_new > eps_inappropriate:
+        eps_new = 0.5 * (eps_inappropriate + eps_appropriate)
+    end
 
-        #if dialog:
-        #    word = 'bisection' if (not no_divergences) else 'update'
-        #    print('varE / varE wanted: {} ---'.format(np.round(varE / varE_wanted, 4)) + word + '---> eps: {}, sigma = L / sqrt(d): {}'.format(np.round(eps_new, 3), np.round(L_new / np.sqrt(self.Target.d), 3)))
+    self.set_hyperparameters(L_new, eps_new)
 
-        return key_new, eps_inappropriate, eps_appropriate, success
+    #if dialog:
+    #    word = 'bisection' if (not no_divergences) else 'update'
+    #    print('varE / varE wanted: {} ---'.format(np.round(varE / varE_wanted, 4)) + word + '---> eps: {}, sigma = L / sqrt(d): {}'.format(np.round(eps_new, 3), np.round(L_new / np.sqrt(self.Target.d), 3)))
+
+    return eps_inappropriate, eps_appropriate, success
 end
 
-function tune_hyperparameters(sett:Settings, target::Target; kwargs...)
-        kwargs = Dict(kwargs)
+function tune_hyperparameters(init, sampler::Sampler, target::Target; kwargs...)
+        sett = sampler.settings
 
         # targeted energy variance per dimension
-        varE_wanted = get(kwargs, :varE_wanted, 0.0005)
-        burn_in = get(kwargs, :burn_in, 2000)
-        samples = get(kwargs, :samples, 1000)
+        varE_wanted = sett.tune_varE_wanted
+        burn_in = sett.tune_burn_in
+        samples = sett.tune_samples
 
         ### debugging tool ###
         dialog = get(kwargs, :dialog, false)
@@ -79,9 +83,10 @@ function tune_hyperparameters(sett:Settings, target::Target; kwargs...)
         key = sett.key
 
         init_eps, init_L = np.sqrt(target.d), 0.6
-        init_nu = sqrt((exp(2 * init_eps / init_L) - 1.0) / target.d)
+        for i in 1:kwargs[:nadaptation]
+            init, x0 = Step(sampler, target, init)
+        end
 
-        x0 = sample(burn_in, x_initial, random_key=subkey, final_state=true)
         props = (Inf, 0.0, false)
 
         if dialog:
@@ -89,6 +94,7 @@ function tune_hyperparameters(sett:Settings, target::Target; kwargs...)
         end
 
         ### first stage: L = sigma sqrt(d)  ###
+        ### LEFT HERE
         for i in 1:10 # = maxiter
             props = tuning_step(props)
             if props[end]: # success == True
@@ -113,9 +119,12 @@ function tune_hyperparameters(sett:Settings, target::Target; kwargs...)
             if n[i] > 10.0 / ESS:
                 break
 
-        L = 0.4 * self.eps / ESS # = 0.4 * correlation length
-        self.set_hyperparameters(L, self.eps)
+        L = 0.4 * eps / ESS # = 0.4 * correlation length
 
         if dialog:
-            print('L / sqrt(d) = {}, ESS(correlations) = {}'.format(L / np.sqrt(self.Target.d), ESS))
+            print('L / sqrt(d) = {}, ESS(correlations) = {}'.format(L / sqrt(target.d), ESS))
             print('-------------')
+        end
+
+        return eps, L
+end
