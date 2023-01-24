f8f7fbf5c7ad2b0dd1673120edd1236bf8aacc16
jaimerz <jaimerz011235813@gmail.com>
2023-01-24 10:48:29 -0800

    add r to all dynamics outputs

--- src/integrators.jl
+++ src/integrators.jl
@@ -1,36 +1,36 @@
-function Leapfrog(sampler::Sampler, target::Target, x, g, u)
+function Leapfrog(sampler::Sampler, target::Target, x, g, u, r)
     sett = sampler.settings
 
-    uu = Update_momentum(sampler, target, sett.eps * 0.5, g, u)
+    uu, rr = Update_momentum(sampler, target, sett.eps * 0.5, g, u, r)
 
     #full step in x
     xx = x .+ sett.eps .* uu
     gg = target.grad_nlogp(xx) .* target.d ./ (target.d - 1)
 
     #half step in momentum
-    uu = Update_momentum(sampler, target, sett.eps * 0.5, gg, uu)
+    uu, rr = Update_momentum(sampler, target, sett.eps * 0.5, gg, uu, rr)
 
-    return xx, gg, uu
+    return xx, gg, uu, rr
 end
 
 
-function Minimal_norm(sampler::Sampler, target::Target, x, g, u)
+function Minimal_norm(sampler::Sampler, target::Target, x, g, u, r)
     """Integrator from https://arxiv.org/pdf/hep-lat/0505020.pdf, see Equation 20."""
 
     # V T V T V
     sett = sampler.settings
 
-    uu = Update_momentum(sampler, target, sett.eps * sett.lambda_c, g, u)
+    uu, rr = Update_momentum(sampler, target, sett.eps * sett.lambda_c, g, u, r)
 
     xx = x .+ sett.eps .* 0.5 .* uu
     gg = target.grad_nlogp(xx) .* target.d ./ (target.d - 1)
 
-    uu = Update_momentum(sampler, target, sett.eps * (1 - 2 * sett.lambda_c), gg, uu)
+    uu, rr = Update_momentum(sampler, target, sett.eps * (1 - 2 * sett.lambda_c), gg, uu, rr)
 
     xx = xx .+ sett.eps .* 0.5 .* uu
     gg = target.grad_nlogp(xx) .* target.d / (target.d - 1)
 
-    uu = Update_momentum(sampler, target, sett.eps * sett.lambda_c, gg, uu)
+    uu, rr = Update_momentum(sampler, target, sett.eps * sett.lambda_c, gg, uu, rr)
 
-    return xx, gg, uu
+    return xx, gg, uu, rr
 end
