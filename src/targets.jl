19f2335c14de94203c1dd32c9929785be26dbcb0
jaimerz <jaimerz011235813@gmail.com>
2023-01-31 13:15:54 -0800

    transform fucntions

--- src/targets.jl
+++ src/targets.jl
@@ -1,8 +1,9 @@
-struct TuringTarget <: Target
+mutable struct TuringTarget <: Target
     d::Int
     nlogp::Function
     grad_nlogp::Function
     transform::Function
+    inv_transform::Function
     prior_draw::Function
 end
 
@@ -25,6 +26,22 @@ TuringTarget(model; kwargs...) = begin
     #     and merge nlogp and grad_nlogp into one function.
     #∂lπ∂θ(x) = LogDensityProblems.logdensity_and_gradient(ℓ, x)
 
+    #function transform(xs)
+    #    dists = _get_dists(vi)
+    #    xxs = [invlink(dist, x) for (dist, x) in zip(dists, xs)]
+    #    return xxs
+    #end
+
+    function transform(x)
+        xt = x
+        return xt
+    end
+
+    function inv_transform(xt)
+        x = xt
+        return x
+    end
+
     function nlogp(x)
         return -1.0 .* ℓπ(x)
     end
@@ -35,32 +52,26 @@ TuringTarget(model; kwargs...) = begin
         #return ∂lπ∂θ(x)[2]
     end
 
-    #function transform(xs)
-    #    dists = _get_dists(vi)
-    #    xxs = [invlink(dist, x) for (dist, x) in zip(dists, xs)]
-    #    return xxs
-    #end
-
-    function transform(x)
-        return x
-    end
-
     function prior_draw(key)
-        return vi[DynamicPPL.SampleFromPrior()]
+        x = vi[DynamicPPL.SampleFromPrior()]
+        xt = transform(x)
+        return xt
     end
 
     TuringTarget(d,
                nlogp,
                grad_nlogp,
                transform,
+               inv_transform,
                prior_draw)
 end
 
-struct CustomTarget <: Target
+mutable struct CustomTarget <: Target
     d::Int
     nlogp::Function
     grad_nlogp::Function
     transform::Function
+    inv_transform::Function
     prior_draw::Function
 end
 
@@ -73,31 +84,51 @@ CustomTarget(nlogp, grad_nlogp, priors; kwargs...) = begin
     #end
 
     function transform(x)
+        xt = x
+        return xt
+    end
+
+    function inv_transform(xt)
+        x = xt
         return x
     end
 
     function prior_draw(key)
-        return [rand(key, dist) for dist in priors]
+        x = [rand(key, dist) for dist in priors]
+        xt = transform(x)
+        return xt
     end
 
     CustomTarget(d,
                nlogp,
                grad_nlogp,
                transform,
+               inv_transform,
                prior_draw)
 end
 
-struct StandardGaussianTarget <: Target
+mutable struct StandardGaussianTarget <: Target
     d::Int
     nlogp::Function
     grad_nlogp::Function
     transform::Function
+    inv_transform::Function
     prior_draw::Function
 end
 
 StandardGaussianTarget(; kwargs...) = begin
 
     d = kwargs[:d]
+
+    function transform(x)
+        xt = x
+        return xt
+    end
+
+    function inv_transform(xt)
+        x = xt
+        return x
+    end
 
     function nlogp(x)
         return 0.5 * sum(x.^2)
@@ -107,24 +138,23 @@ StandardGaussianTarget(; kwargs...) = begin
         return ForwardDiff.gradient(nlogp, x)
     end
 
-    function transform(x)
-        return x
-    end
-
     function prior_draw(key)
         mean = zeros(d)
         variance = ones(d)
-        return 4*rand(key, MvNormal(mean, variance))
+        x = 4*rand(key, MvNormal(mean, variance))
+        xt = transform(x)
+        return xt
     end
 
     StandardGaussianTarget(kwargs[:d],
                            nlogp,
                            grad_nlogp,
                            transform,
+                           inv_transform,
                            prior_draw)
 end
 
-struct CMBLensingTarget <: Target
+mutable struct CMBLensingTarget <: Target
     d::Int
     Λmass
     nlogp::Function
