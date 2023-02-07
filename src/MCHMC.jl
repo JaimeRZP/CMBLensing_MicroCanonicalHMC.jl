f56e163864723a6e822f4ecf7b38bceb97e22f41
jaimerz <jaimerz011235813@gmail.com>
2023-02-07 15:49:52 -0800

    ess_corr WIP

--- src/MCHMC.jl
+++ src/MCHMC.jl
@@ -5,7 +5,7 @@ export Leapfrog, Minimal_norm
 export TuringTarget, StandardGaussianTarget, CustomTarget, CMBLensingTarget
 
 using Interpolations, LinearAlgebra, Statistics, DynamicPPL, Turing, DataFrames
-using Distributions, Random, ForwardDiff, Zygote, AbstractMCMC
+using Distributions, Random, ForwardDiff, Zygote, AbstractMCMC, FFTW
 using LogDensityProblems, LogDensityProblemsAD, Bijectors
 
 abstract type Target end
--- src/tuning.jl
+++ src/tuning.jl
@@ -1,29 +1,29 @@
+#=
 function ess_corr(samples)
     """Taken from: https://blackjax-devs.github.io/blackjax/diagnostics.html
         shape(x) = (num_samples, d)"""
     input_array = samples.Ω
     num_samples = length(input_array)
-    #
     mean_across_chain = mean(input_array)
     # Compute autocovariance estimates for every lag for the input array using FFT.
-    centered_array = input_array - mean_across_chain
-    m = next_fast_len(2 * num_samples)
-    ifft_ary = jnp.fft.rfft(centered_array, n=m, axis=1)
-    ifft_ary *= jnp.conjugate(ifft_ary)
-    autocov_value = jnp.fft.irfft(ifft_ary, n=m, axis=1)
-    autocov_value = (
-        jnp.take(autocov_value, jnp.arange(num_samples), axis=1) / num_samples
-    )
-    mean_autocov_var = autocov_value.mean(0, keepdims=True)
-    mean_var0 = (jnp.take(mean_autocov_var, jnp.array([0]), axis=1) * num_samples / (num_samples - 1.0))
-    weighted_var = mean_var0 * (num_samples - 1.0) / num_samples
-    weighted_var = jax.lax.cond(num_chains > 1,
-                                lambda _: weighted_var+ mean_across_chain.var(axis=0, ddof=1, keepdims=True),
-                                lambda _: weighted_var,
-                                operand=None)
+    centered_array = mapreduce(permutedims, vcat, input_array - mean_across_chain)
+
+    #m = next_fast_len(2 * num_samples)
+    ifft_ary = fft(centered_array)
+    ifft_ary *= conj(ifft_ary)
+    autocov_value = ifft(ifft_ary, n=m, axis=1)
+    autocov_value ./ num_samples #(jnp.take(autocov_value, jnp.arange(num_samples), axis=1) / num_samples)
+    mean_autocov_var = mean(autocov_value)
+    #mean_var0 = (jnp.take(mean_autocov_var, jnp.array([0]), axis=1) * num_samples / (num_samples - 1.0))
+    #weighted_var = mean_var0 * (num_samples - 1.0) / num_samples
+    #weighted_var = jax.lax.cond(num_chains > 1,
+    #                            lambda _: weighted_var+ mean_across_chain.var(axis=0, ddof=1, keepdims=True),
+    #                            lambda _: weighted_var,
+    #                            operand=None)
+
     # Geyer's initial positive sequence
-    num_samples_even = num_samples - num_samples % 2
-    mean_autocov_var_tp1 = jnp.take(mean_autocov_var, jnp.arange(1, num_samples_even), axis=1)
+    num_samples_even = num_samples - mod(num_samples, 2)
+    mean_autocov_var_tp1 = mean_auto_cov[1:num_samples_even] #jnp.take(mean_autocov_var, jnp.arange(1, num_samples_even), axis=1)
     rho_hat = jnp.concatenate([jnp.ones_like(mean_var0), 1.0 - (mean_var0 - mean_autocov_var_tp1) / weighted_var,], axis=1,)
     rho_hat = jnp.moveaxis(rho_hat, 1, 0)
     rho_hat_even = rho_hat[0::2]
@@ -69,6 +69,7 @@ function ess_corr(samples)
     neff = ess.squeeze() / num_samples
     return 1.0 / mean(1 / neff)
 end
+=#
 
 function tune_eps(sampler::Sampler, target::Target, init; kwargs...)
     dialog = get(kwargs, :dialog, false)
