ca479827f4faa8f1100acf0fd0ba79df598e64fc
jaimerz <jaimerz011235813@gmail.com>
2023-02-10 10:11:51 -0800

    compilling

--- src/MicroCanonicalHMC.jl
+++ src/MicroCanonicalHMC.jl
@@ -6,7 +6,7 @@ export TuringTarget, StandardGaussianTarget, CustomTarget, CMBLensingTarget
 
 using Interpolations, LinearAlgebra, Statistics, Distributions, Random, DataFrames
 using DynamicPPL, Turing, LogDensityProblemsAD, LogDensityProblems, ForwardDiff, Zygote
-using AbstractMCMC, MCMCChains
+using AbstractMCMC, MCMCChains, Distributed
 
 abstract type Target <: AbstractMCMC.AbstractModel end
 
