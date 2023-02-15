7873a79c7ed8bfd5de14e4cf51ee50cf1b21c477
jaimerz <jaimerz011235813@gmail.com>
2023-02-14 16:29:21 -0800

    resume working

--- src/abstractmcmc_parallel.jl
+++ src/abstractmcmc_parallel.jl
@@ -16,38 +16,38 @@ function AbstractMCMC.sample(model::DynamicPPL.Model,
         state, sample = init
         # We will have to parallelize this later
         tune_hyperparameters(sampler, target, state; kwargs...)
+
+        if nchains < Threads.nthreads()
+            @info string("number of chains: ",
+                         nchains,
+                         " smaller than number of threads: ",
+                         Threads.nthreads(),  ".",
+                         " Increase the number of chains to make full use of your threads.")
+        end
+        if nchains > Threads.nthreads()
+            @info string("number of chains: ",
+                         nchains,
+                         " requesteed larger than number of threads: ",
+                         Threads.nthreads(),  ".",
+                         " Setting number of chains to number of threads.")
+            nchains = Threads.nthreads()
+        end
+
+        interval = 1:nchains
+        chains = Vector{MCMCChains.Chains}(undef, nchains)
+        targets = [deepcopy(target) for _ in interval]
+        samplers = [deepcopy(sampler) for _ in interval]
+        inits = [Get_initial_conditions(sampler, target) for _ in interval]
+
     else
         @info "Starting from previous run"
-        target = resume_from.info[:target]
-        sampler = resume_from.info[:sampler]
-        # In the parallel case we need a different
-        # init for each worker
-        #init = resume_from.info[:init]
+        nchains = length(resume_from)
+        interval = 1:nchains
+        chains = Vector{MCMCChains.Chains}(undef, nchains)
+        targets = [chain.info[:target] for chain in resume_from]
+        samplers = [chain.info[:sampler] for chain in resume_from]
+        inits = [chain.info[:init] for chain in resume_from]
     end
-
-    # Start parallelization
-    if nchains < Threads.nthreads()
-        @info string("number of chains: ",
-                     nchains,
-                     " smaller than number of threads: ",
-                     Threads.nthreads(),  ".",
-                     " Increase the number of chains to make full use of your threads.")
-    end
-    if nchains > Threads.nthreads()
-        @info string("number of chains: ",
-                     nchains,
-                     " requesteed larger than number of threads: ",
-                     Threads.nthreads(),  ".",
-                     " Setting number of chains to number of threads.")
-        nchains = Threads.nthreads()
-    end
-    interval = 1:nchains
-    chains = Vector{MCMCChains.Chains}(undef, nchains)
-    targets = [deepcopy(target) for _ in interval]
-    samplers = [deepcopy(sampler) for _ in interval]
-    # when restarting from an old run
-    # these we need to get the inits from that run
-    inits = [Get_initial_conditions(sampler, target) for _ in interval]
 
     @AbstractMCMC.ifwithprogresslogger progress name = progressname begin
         # Create a channel for progress logging.
@@ -73,5 +73,5 @@ function AbstractMCMC.sample(model::DynamicPPL.Model,
         end
     end
 
-    return chainsstack(chains)
+    return chains
 end
