d605a654e4d5360fe03da39302d7424dd071da70
jaimerz <jaimerz011235813@gmail.com>
2023-03-23 20:37:00 -0700

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,10 @@
 # make.jl
 using Documenter, GaussianProcess
 
-makedocs(sitename = "MCHMC.jl",
-         modules = [GaussianProcess],
+makedocs(sitename = "MicroCanonicalHMC.jl",
+         modules = [MiCroCanonicalHMC],
          pages = ["Home" => "index.md",
                   "API" => "api.md"])
 deploydocs(
-    repo = "github.com/JaimeRZP/MCHCM.jl"
+    repo = "github.com/JaimeRZP/MicroCanonicalHMC.jl"
 )
