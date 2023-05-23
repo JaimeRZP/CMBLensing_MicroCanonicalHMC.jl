6a3912d4130056180de8515ae483995e56f1fe33
jaimerz <jaimerz011235813@gmail.com>
2023-05-23 11:40:07 +0100

    summ matrix

--- src/tuning.jl
+++ src/tuning.jl
@@ -46,6 +46,14 @@ function Summarize(samples::AbstractVector)
     return ess, rhat
 end
 
+function Summarize(samples::AbstractMatrix)
+    dim_a, dim_b = size(samples)
+    _samples = zeros(dim_a, 1, dim_b)
+    _samples[:, 1, :] = samples
+    ess, rhat = MCMCDiagnosticTools.ess_rhat(_samples)
+    return ess, rhat
+end
+
 function Neff(samples, l::Int)
     ess, rhat = Summarize(samples)
     neff = ess ./ l
