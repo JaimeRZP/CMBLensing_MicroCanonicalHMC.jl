1f55a867105432b78a55225f9737b85caefff2d3
jaimerz <jaimerz011235813@gmail.com>
2023-07-03 15:35:33 +0100

    Revert "quicker tests"
    This reverts commit 3f74cd9aa46ca37ce6c4fb759e3fa7eecf7a0617.

--- test/models.jl
+++ test/models.jl
@@ -11,8 +11,9 @@
         target = GaussianTarget(m, cov_matt)
 
         spl = MCHMC(10_000, 0.01; init_eps=sqrt(d))
-        samples_mchmc = Sample(spl, target, 100_000);
-        samples_mchmc_adaptive = Sample(spl, target, 100_000; adaptive=true);
+        samples_mchmc = Sample(spl, target, 100_000; dialog=true);
+        samples_mchmc_adaptive = Sample(spl, target, 100_000;
+            adaptive=true, dialog=true);
 
         _samples_mchmc = mapreduce(permutedims, vcat, samples_mchmc)
         s1 = std(_samples_mchmc, dims=1)[1:end-3]
@@ -32,7 +33,7 @@
         ##############
         ### Neal's ###
         ##############
-        d = 10
+        d = 21
         @model function funnel()
             θ ~ Truncated(Normal(0, 3), -3, 3)
             z ~ MvNormal(zeros(d-1), exp(θ)*I)
@@ -46,22 +47,22 @@
         target = TuringTarget(funnel_model; d=d, compute_MAP=false)
 
         spl = MCHMC(1_000, 0.01)
-        samples_mchmc = Sample(spl, target, 10_000;)
+        samples_mchmc = Sample(spl, target, 10_000; dialog=false)
 
         theta_mchmc = [sample[1] for sample in samples_mchmc]
-        x10_mchmc = [sample[2] for sample in samples_mchmc]
+        x10_mchmc = [sample[10+1] for sample in samples_mchmc]
         mm1, m1, s1 = (median(theta_mchmc), mean(theta_mchmc), std(theta_mchmc))
         mm2, m2, s2 = (median(x10_mchmc), mean(x10_mcjmc), std(x10_mchmc))
         E = [sample[end-1] for sample in samples_mchmc];
         VarE = std(E)^2/d
 
-        @test VarE ≈ 0.01 atol=0.005
-        @test mm1 ≈ -0.06 atol=0.01
-        @test m1 ≈ -0.19 atol=0.01
-        @test s1 ≈ 1.04 atol=0.1
-        @test mm2 ≈ -0.17 atol=0.1
-        @test m2 ≈ 0.20 atol=0.1
-        @test s2 ≈ 0.70 atol=0.1
+        @test VarE ≈ 0.01 atol=0.03
+        @test mm1 ≈ 0.07 atol=0.01
+        @test m1 ≈ 0.01 atol=0.01
+        @test s1 ≈ 0.72 atol=0.1
+        @test mm2 ≈ -0.82 atol=0.1
+        @test m2 ≈ -0.86 atol=0.1
+        @test s2 ≈ 0.76 atol=0.1
     end
 
     @testset "Rosembrok" begin
@@ -71,7 +72,7 @@
         rng = MersenneTwister(1234)
         target = RosenbrockTarget(1.0, 10.0; d=2)
         spl = MCHMC(10_000, 0.01; L=sqrt(2), sigma=ones(target.d), adaptive=true)
-        samples = Sample(rng, spl, target, 200_000)
+        samples = Sample(rng, spl, target, 200_000; dialog=true)
         d1 = [sample[1] for sample in samples]
         d2 = [sample[2] for sample in samples]
         mm1, m1, s1, = (median(d1), mean(d1), std(d1))
