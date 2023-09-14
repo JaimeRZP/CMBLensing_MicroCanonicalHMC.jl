ff7ac3b53314b15d03c5964843cf886a50bad593
jaimerz <jaimerz011235813@gmail.com>
2023-09-14 10:04:28 +0100

    adapted script

--- scripts/run_mchmc.sh
+++ scripts/run_mchmc.sh
@@ -8,17 +8,9 @@
 #SBATCH --nodes 8
 #SBATCH -A mp107
 #=
-​
 srun /global/homes/j/jaimerz/.julia/juliaup/julia-1.9.0-rc2+0.x64.linux.gnu/bin/julia $(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}')
 exit 0
 ​
 # =#
-​
-using ParameterizedNotebooks
-ParameterizedNotebook("pipeline/03_get_MAP_or_MUSE.ipynb", sections=("Init","MPI","Pool"), recursive=false)()
-ParameterizedNotebook("pipeline/03_get_MAP_or_MUSE.ipynb", sections=("Params","Bandpowers","New result", "get_J", "get_H"), recursive=false)(
-    nsims = 200,
-    data_type = RealData(),
-    sims_type = (LensPyxSignal(; Nside=2048, epsilon=1e-6, projector_storage=Array), SignFlipNoise()),
-)
+MCHMC.jl
 CMBLensing.stop_MPI_workers()
