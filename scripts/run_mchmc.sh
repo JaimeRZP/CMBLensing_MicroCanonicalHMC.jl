69e650a8e6dcc2a7f97f3b54f3df9e552752a107
jaimerzp <jaimerz011235813@gmail.com>
2023-09-14 02:27:50 -0700

    right project

--- scripts/run_mchmc.sh
+++ scripts/run_mchmc.sh
@@ -2,15 +2,14 @@
 #SBATCH -C gpu 
 #SBATCH -q regular
 #SBATCH -t 06:00:00 
-#SBATCH --cpus-per-task 32 
+#SBATCH --cpus-per-task 4 
 #SBATCH --gpus-per-task 1 
-#SBATCH --ntasks-per-node 4
-#SBATCH --nodes 8
-#SBATCH -A mp107
+#SBATCH --ntasks-per-node 1
+#SBATCH --nodes 1
+#SBATCH -A m4031
 #=
 srun /global/homes/j/jaimerz/.julia/juliaup/julia-1.9.0-rc2+0.x64.linux.gnu/bin/julia $(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}')
 exit 0
-​
 # =#
 MCHMC.jl
 CMBLensing.stop_MPI_workers()
