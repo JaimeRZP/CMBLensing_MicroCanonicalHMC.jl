fa2287c69cac372be6fcc4dca2277980953e89be
jaimerz <jaimerz011235813@gmail.com>
2023-10-19 10:21:45 +0100

    tes

--- scripts/HMC.jl
+++ scripts/HMC.jl
@@ -38,6 +38,7 @@ end
 
 #Sampler
 ϵ=0.0025
+iterations = 100
 Ω = prob.Ωstart
 samples_hmc = []
 rng = Xoshiro(1)
@@ -49,7 +50,6 @@ deriv_precond.θ.r = 5.85
 deriv_precond.θ.Aϕ = 112.09
 Λmass_new = Diagonal(FieldTuple(f°=diag(new_f), ϕ°=diag(prob.Λmass[:ϕ°]), θ=deriv_precond.θ));
 
-iterations = 10_000
 @showprogress for i=1:iterations
     Ω, = state = hmc_step(rng, prob, prob.Ωstart, new_Λmass; symp_kwargs=[(N=25, ϵ=ϵ)], progress=false, always_accept=(i<10))
     push!(samples_hmc, adapt(Array, state))
